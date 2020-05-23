131b784c274b663c316030ce6c8e8f6209072079
EC2 Default User <ec2-user@ip-172-31-22-53.ap-southeast-1.compute.internal>
2020-05-23 13:03:08 +0000

    Cleanup

--- installDep.sh
+++ installDep.sh
@@ -1,6 +1,4 @@
 source /home/ec2-user/.bash_profile
-cd /tmp/lol/test-nodejs
+cd /tmp/demo/nodejs-codedeploy
 npm install
 pwd
-echo "The following files are here:"
-ls
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 source /home/ec2-user/.bash_profile
-cd /tmp/lol/test-nodejs
+cd /tmp/demo/nodejs-codedeploy
 pm2 delete all
 pm2 start index.js -f
 pwd
